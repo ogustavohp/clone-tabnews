5a75986bc5877c742395ff69e61b4d18dce57c86
Gustavo Henrique <107812402+ogustavohp@users.noreply.github.com>
2024-06-19 18:59:08 +0000

    add: scripts lint:check  lint:fix

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Recriação educacional do https://www.tabnews.com.br para fins de aprendizado e prática.
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
-  return (
-    <h1>Opa, bão!?</h1>
-  )
+  return <h1>Opa, bão!?</h1>;
 }
-export default Home
+
+export default Home;
